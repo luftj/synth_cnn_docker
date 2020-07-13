e13c7c69767f390f22c49a40eda43ae336a6502e
Jonas <jonas.luft@hcu-hamburg.de>
2020-07-13 14:44:25 +0200

    fixed run_test

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,6 @@
 docker run --name cnn_synth_test -ti --rm \
-    -p 8008:8008 \
-    --gpus all \
+    --gpus device=1 \
     -v "$(pwd)"/cnn_lstm_ctc_ocr/data/model/:/app/cnn_lstm_ctc_ocr/data/model/ \
     --entrypoint python \
     cnn_synth \
-    test.pygit a
+    test.py
