f18a93deb072317e6b80da739719f1d2872ba769
Jonas <jonas.luft@hcu-hamburg.de>
2020-07-15 08:29:13 +0200

    preparation validate script

--- resize.sh
+++ resize.sh
@@ -1,4 +1,7 @@
+inputpath=$1
+cd $inputpath
 convert *.png -resize x31 -set filename:base "%[basename]" "%[filename:base]_32.png"
 rm imagelist.txt
 touch imagelist.txt
 ls *_32.png > imagelist.txt
+sed 's/^/\/app\/images\//' imagelist.txt
--- validate.sh
+++ validate.sh
@@ -1,6 +1,15 @@
+if [ "$#" = 0 ];
+then
+    echo "please supply a input path as argument"
+    exit
+fi
+
+inputpath=$1
+./resize.sh $inputpath
+
 docker run --name cnn_synth_val -ti --rm \
     -v "$(pwd)"/cnn_lstm_ctc_ocr/data/model:/app/cnn_lstm_ctc_ocr/data/model \
-    -v "$(pwd)"/images:/app/images/ \
+    -v "$(pwd)"/$inputpath:/app/images/ \
     --entrypoint bash \
     cnn_synth \
     /app/pyval.sh
